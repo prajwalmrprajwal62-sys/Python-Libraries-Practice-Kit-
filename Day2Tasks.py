4fe46983533d4f829f15c2b8ec39d22e6d911cfd
Prajwal M R <prajwalmrprajwal62@gmail.com>
2026-08-01 22:58:49 +0530

    commit

--- Day2Tasks.py
+++ Day2Tasks.py
@@ -1,5 +1,11 @@
 import pandas as pd
-df = pd.read_csv('students.csv')
 
-print(df['passed'].value_counts())
-print('Gender=',df['gender'].nunique())
+df2 = pd.read_csv('attendance.csv')
+df1 = pd.read_csv('students.csv')
+
+print(df1['passed'].value_counts())
+print('Gender=',df1['gender'].nunique())
+
+print(df1.merge(df2, on='name'))
+print(df1.merge(df2, on='name', how='inner'))
+print(df1.merge(df2, on='name', how='left'))
